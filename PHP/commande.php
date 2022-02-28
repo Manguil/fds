50685bfa4b32986ebbf7404a31235b3f43ec3042
Manguil <49762113+Manguil@users.noreply.github.com>
2022-02-28 22:56:38 +0100

    add upload

--- PHP/commande.php
+++ PHP/commande.php
@@ -1,40 +1,63 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ticket commande</title>
 </head>
+
 <body>
-    <h1>Commande</h1> <br>
-    <table>
-        <tr>
-            <th>Quantité</th>
-            <th>Article</th>
-            <th>Prix</th>
-        </tr>
-        <?php
-            $prixArticle=array('marteau'=>10, 'tenaille'=>5, 'vis'=>5.2, 'clou'=>5.8,'tournevis'=>7, 'ciseau'=>4, 'toile émeri'=>3);
-            if (!empty($_GET["objet"]) && !empty($_GET["n"])){
-                var_dump($_GET["article"]);
-                if (!empty($_GET["article"])){
-                    $article = $_GET["article"][0];
-                    $n = $_GET["article"][1];
-                    $prix = $prixArticle[$article]*$n;
-                    echo "<tr><td>$n</td><td>$article</td><td>$prix €</td></tr>";
-                }
-                else echo "non";
-                $n = $_GET["n"];
-                $article = $_GET["objet"];
-                $prix = $prixArticle[$article]*$n;   
-                echo "<tr><td>$n</td><td>$article</td><td>$prix €</td></tr>";
-                echo "<input type=text name=article value=array($article,$n)";
-            };
-        ?>
-    </table>
-    <hr>
     <form name='commande' method='get'>
+        <h1>Commande</h1> <br>
+        <table>
+            <tr>
+                <th>Quantité</th>
+                <th>Article</th>
+                <th>Prix</th>
+            </tr>
+            <?php
+            $total = 0;
+            $prixArticle = array('marteau' => 10, 'tenaille' => 5, 'vis' => 5.2, 'clou' => 5.8, 'tournevis' => 7, 'ciseau' => 4, 'toile émeri' => 3);
+            foreach (array_keys($prixArticle) as $value) {
+                if (isset($_GET[$value])) {
+                    if (isset($_GET["objet"]) && isset($_GET["n"])) {
+                        if ($_GET["objet"] == $value) {
+                            $n = $_GET["n"];
+                            $article = $_GET["objet"];
+                            $prix = $prixArticle[$article] * ($_GET[$value] + $n);
+                            $total += $prix;
+                            echo '<input type="hidden" name="' . $value . '" value="' . $_GET[$value] + $n . '">';
+                            echo '<tr><td>' . $_GET[$value] + $n . '</td><td>' . $article . '</td><td>' . $prix . ' €</td></tr>';
+                        } else {
+                            $n = $_GET[$value];
+                            if ($n > 0) {
+                                $article = $value;
+                                $prix = $prixArticle[$article] * $n;
+                                $total += $prix;
+                                echo '<tr><td>' . $n . '</td><td>' . $article . '</td><td>' . $prix . ' €</td></tr>';
+                            };
+                            echo '<input type="hidden" name="' . $value . '" value="' . $n . '">';
+                        }
+                    };
+                } else {
+                    echo '<input type="hidden" name="' . $value . '" value="0">';
+                };
+            }
+            echo '<tr><td><th>TOTAL sans reduc</th></td><td>' . $total . ' €</td></tr>';
+            if (isset($_GET["codeReduc"])){
+                $code = $_GET["codeReduc"];
+                if ($code == "DIXPOURCENT"){
+                    echo '<tr><td><th>TOTAL avec reduc</th></td><td>' . $total - ($total*(10/100)) . ' €</td></tr>';
+                };
+            };
+            ?>
+        </table>
+        <label for="codeReduc">Rentrer un code promo : </label>
+        <input type="text" name="codeReduc" value="<?php if (isset($_GET["codeReduc"])){
+                echo $_GET["codeReduc"];}; ?>">
+        <hr>
         <p>Nouvel article :</p>
         <select name="objet" id="objet">
             <option value="marteau">marteau</option>
@@ -45,9 +68,10 @@
             <option value="ciseau">ciseau</option>
             <option value="toile émeri">toile émeri</option>
         </select>
-        <input type="number" name="n" id="n" >
+        <input type="number" name="n" id="n">
         <button type="submit">Ajouter</button>
-        
+
     </form>
 </body>
+
 </html>
