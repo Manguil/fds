106e3b4ad69682d54534458f8f07106e00ec0f55
Manguil <49762113+Manguil@users.noreply.github.com>
2022-03-01 20:18:09 +0100

    Update OL_OSM.js

--- html/TD_TP3/OL_OSM.js
+++ html/TD_TP3/OL_OSM.js
@@ -5,49 +5,26 @@ $().ready(function () {
         console.log("liste de type de batiment : ",listTypes);
         console.log("nombre de batiment : ",nbBat);
         let html = "<div id=points_interet>";
-        let numero = 0;
+        let numero = 1;
         for (let type of listTypes) {
             console.log("type : ", type);
             $.getJSON("http://localhost:8888/type/" + type, function (listBat) {
                 console.log(type);
-                html += "<h3>" + type + "</h3>" + "<div id=" + type + ">";
+                html += "<h3>" + type + "</h3>" + "<div id=" + "'" + type + "'" + ">";
 
                 for (elem of listBat) {
                     html += "<input type=checkbox name='" + numero + "'>" + elem["nom"] + "</input>";
                     numero++;
                 }
-                console.log(numero);
 
                 html += "</div>";
-                if (numero == nbBat) {
+                if (numero == nbBat+1) {
                     html += "</div>";
                     console.log("le code html est ", html);
                     $("#points_interet").append(html);
+                    $('#points_interet').accordion({collapsible: true, heightStyle: 'content'});
                 }
             });
         }
     });
 });
-
-/**$().ready(function () {
-    $.getJSON("http://localhost:8888/types", function (listTypes) {
-        let nbTypes = 0;
-        $.getJSON("http://localhost:8888/type/" + listTypes[nbTypes], function (listBat) {
-            for (type of listTypes) {
-                let html = "";
-                let numero = 0;
-                console.log(type);
-                html += "<div id=points_interet> <h3>" + type + "</h3>" +
-                    "<div id=" + type + ">";
-                for (elem of listBat) {
-                    html += "<input type=checkbox name='" + numero + "'>" + elem["nom"] + "</input>"
-                    numero++;
-                }
-                console.log(html);
-                $("#points_interet").append(html);
-                nbTypes++;
-                html += "</div>";
-            }
-        });
-    });
-}); */
