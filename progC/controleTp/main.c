3dc959ea6222d5f4117a16f513aaf8403367b965
Manguil <scorpion34700normand@gmail.com>
2022-03-11 09:35:54 +0100

    Add

--- progC/controleTp/main.c
+++ progC/controleTp/main.c
@@ -11,7 +11,8 @@ int main(){
 		case 1:{
 			nb = saisieEntier(); nb1 = saisieEntier();
 			printf("somme = %d\n", addition(nb,nb1));
-			break;}
+			break;
+			}
 		
 		case 2:{
 			nb = saisieEntier(); nb1 = saisieEntier();
@@ -23,7 +24,7 @@ int main(){
 		case 3 :{
 			/*nb = saisieEntier(); nb1 = saisieEntier();
 			cEntier couple = {nb,nb1}; */
-			//cEntier couple = {0,0};
+			cEntier couple = {0,0};
             printf("donne un couple N*N (format: (a,b))");
 			scanf("(%d,%d)",&couple.Entier1,&couple.Entier2);
 			printf("max = %d\n", monMax(couple));
