08caab5a4dceb433ba4a3fbff166fb005fa44d08
Manguil <scorpion34700normand@gmail.com>
2022-09-27 11:18:24 +0200

    add

--- L2/ProgC/decodeHuffman.c
+++ L2/ProgC/decodeHuffman.c
@@ -1,33 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_SIZE 'Z'+1
 
-char* associe(char* s)
+char* ctos(char c)
 {
-    switch (atoi(s))
-    {
-        case 11 : "A"
-        case 101 : "B"
-        case 100 : "C"
-        default : "\0"
-    }
+    char* s = malloc(1);
+    s[0] = c;
+    return s;
 }
 
-char* decode(char* s)
+
+char* encoder(char** codage, char* s) 
 {
-    int i = 0;
-    char* temp = ""
-    char* res = "";
-    while (s!=0)
+    char* result = malloc(sizeof(char)*strlen(s));
+    for(int i=0; s[i] != 0; i++)
     {
-        temp = associe(s[i]);
-        if (temp!=0){
-            strcat(res,temp);
-        else return "pas possible de decoder";
+        strcat(result, codage[s[i]]);
+    }
+    return result;
+}
+
+
+char* decoder(char** codage, char* s) 
+{
+    char* temp = malloc(sizeof(char)*3);
+    char* result = malloc(sizeof(char)*strlen(s)/2);
+    int i=0;
+    while(s[i] != 0)
+    {
+        for(int j='A'; j <= 'Z'; j++)
+        {
+            if (codage[j] == temp)
+            {
+                strcat(result, ctos(j));
+                temp = "";
+                i+=j;
+                break;
+            }
+            strcat(temp, s[i]);
         }
     }
+    return result;
 }
+
+
 
 int main(int argc, char **argv)
 {
-
+    char* codage[MAX_SIZE];
+    for(int i='A'; i<='Z'; i++)
+    {
+        codage[i] = "";
+    }
+    codage['A'] = "01";
+    codage['B'] = "11";
+    codage['C'] = "101";
+    char* s = "CAC";
+    char* result = encoder(codage,s);
+    char* result2 = decoder(codage,s);
+    printf("%s\n", result2);
 }
