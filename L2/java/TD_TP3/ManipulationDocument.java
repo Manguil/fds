719855a41004f2a80eadec7235ea9b46accf662f
Manguil <49762113+Manguil@users.noreply.github.com>
2022-09-29 21:15:49 +0200

    add

--- L2/java/TD_TP3/ManipulationDocument.java
+++ L2/java/TD_TP3/ManipulationDocument.java
@@ -1,8 +1,6 @@
-import java.io.Serial;
-
 public class ManipulationDocument {
     public static void main(String[] args){
         DocumentCrypté d1 = new DocumentCrypté("titre", "texte");
-        System.out.println(d1.decrypter(d1.crypter(d1.getTexte())));
+        System.out.println(DocumentCrypté.decrypter(DocumentCrypté.crypter(d1.getTexte())));
     }
 }
