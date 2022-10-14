140c5eb3f7a3a17ed17d6e821b2388879116268a
Manguil <scorpion34700normand@gmail.com>
2022-10-14 11:17:20 +0200

    add

--- L2/OCaml/projetOCaml.ml
+++ L2/OCaml/projetOCaml.ml
@@ -1,22 +1,93 @@
 type prop =
-| Symb of string
-| Top
-| Bot
-| Not of prop
-| And of prop * prop
-| Or of prop * prop
-| Imp of prop * prop
-| Equ of prop * prop;;
+  | Symb of string
+  | Top
+  | Bot
+  | Not of prop
+  | And of prop * prop
+  | Or of prop * prop
+  | Imp of prop * prop
+  | Equ of prop * prop
+;;
 
 
-let f1 = Equ(And(Symb "a", Symb "b"), Or(Not(Symb "a"), Symb "b")) (*f1 = a∧b ⇔ ¬a∨b*)
-let f2 = Or(Not(And(Symb "a", Not(Symb "b"))), Not(Imp(Symb "a", Symb "b"))) (*f2 = ¬(a∧¬b) ∨ ¬(a⇒b)*)
-let f3 = And(Not(Imp(Symb "a", Or(Symb "a", Symb "b"))), Not(Not(And(Symb "a"), Or(Symb "b", Not(Symb "c"))))) (*f3 = ¬(a ⇒ a∨b) ∧ ¬¬(a∧(b∨¬c))*)
-let f4 = And(And(And(And(And(Or(Or(Not(Symb "a"), Symb "b"), Symb "d")
-		, Or(Not(Symb "d"), Symb "c"))
-		, Or(Not(Symb "c"), Symb "b"))
-		, Or(Not(Symbol "c"), Not(Symbol "b")))
-		, Or(Not(Symb "b"), Symb "d")) (*f4 = (¬a∨b∨d) ∧ (¬d∨c) ∧ (c∨a) ∧ (¬c∨b) ∧ (¬c∨¬b) ∧ (¬b∨d)*)
+(* Q2 *)
+let rec nbc = function
+  | Top -> 0
+  | Bot -> 0
+  | Symb _ -> 0
+  | Not a -> 1 + nbc a
+  | And(a, b) -> 1 + nbc a + nbc b
+  | Or(a, b) -> 1 + nbc a + nbc b
+  | Imp(a, b) -> 1 + nbc a + nbc b
+  | Equ(a, b) -> 1 + nbc a + nbc b
+;;
 
-let rec nbc fbf = function
-| 
+
+(* Q3 *)
+let prof = function
+  | Top -> 0
+  | Bot -> 0
+  | Symb _ -> 0
+  | Not a -> 1 + (nbc a)
+  | And(a, b) -> 1 + max (nbc a) (nbc b)
+  | Or(a, b) -> 1 + max (nbc a) (nbc b)
+  | Imp(a, b) -> 1 + max (nbc a) (nbc b)
+  | Equ(a, b) -> 1 + max (nbc a) (nbc b)
+;;
+
+
+(* Q4 *)
+let rec count n li = match li with 
+  | [] -> 0
+  | t :: q when t = n -> 1 + (count n q)
+  | t :: q -> count n q
+;;
+
+
+let rec supr_doublons = function
+  | [] -> []
+  | t::q when count t q = 0 -> t::supr_doublons q
+  | _::q -> supr_doublons q
+;;
+
+
+let rec creer_li_var = function
+  | Top -> []
+  | Bot -> []
+  | Symb a -> [a]
+  | Not a ->  creer_li_var a
+  | And(a,b) -> (creer_li_var a)@(creer_li_var b)
+  | Or(a,b) -> (creer_li_var a)@(creer_li_var b)
+  | Imp(a,b) -> (creer_li_var a)@(creer_li_var b)
+  | Equ(a,b) -> (creer_li_var a)@(creer_li_var b) 
+;;
+  
+
+let sp fbf = supr_doublons (creer_li_var fbf);;
+
+
+(* Q5 *)
+let rec affiche = function
+  | Symb a -> "a"
+  | Top -> "⊤"
+  | Bot -> "⊥" 
+  | Not a -> "¬" ^ "(" ^  affiche a ^ ")"
+  | And(a, b) -> "(" ^ affiche a ^ "∧" ^ affiche b ^ ")"
+  | Or(a, b) -> "(" ^ affiche a ^ "∨" ^ affiche b ^ ")"
+  | Imp(a, b) -> "(" ^ affiche a ^ "⇒" ^ affiche b ^ ")"
+  | Equ(a, b) -> "(" ^ affiche a ^ "⇔" ^ affiche b ^ ")"
+;;
+
+
+(* Q6 *)
+
+(* Q7 *)
+type valVerite = Zero | Un ;;
+
+type interpretation = (string * valVerite) list;;
+
+let I1 = [("a",Un) ; ("b",Zero) ; ("c",Un)];;
+
+let I2 = [("a",Zero) ; ("b",Zero) ; ("c",Zero)];;
+
+let I3 = [("a",Un) ; ("b",Un) ; ("c",Un)];;
