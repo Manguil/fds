ad46f7dc34a11ed806d98c4177deb1deaf9bc182
Manguil <scorpion34700normand@gmail.com>
2022-10-21 09:32:43 +0200

    add

--- L2/ProgC/exo67.c
+++ L2/ProgC/exo67.c
@@ -1,27 +1,33 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 void parcoursRecursif(char* nomDossier) {
-    
-}
-
-int main(int argc, char **argv){
-    char * nomDossier = argv[1];
     DIR* dossier = opendir(nomDossier);
 
     struct dirent* ent = readdir(dossier);
     while (ent!=NULL) {
         if (ent->d_type == DT_DIR) {
             printf("dossier : %s\n", ent->d_name);
+            if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+                char* chemin = malloc(strlen(nomDossier)+1+strlen(ent->d_name));
+                strcat(chemin,nomDossier);
+                strcat(chemin, "/");
+                strcat(chemin, ent->d_name);
+                parcoursRecursif(chemin);
+            }
         }
         else if (ent->d_type == DT_REG) {
             printf("fichier : %s\n", ent->d_name);
         }
         ent = readdir(dossier);
     }
-    
+}
 
-
+int main(int argc, char **argv){
+    char* nomDossier = argv[1];
+    parcoursRecursif(nomDossier);
     return 0;
 }
