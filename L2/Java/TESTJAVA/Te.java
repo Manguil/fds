66e3baa7b871ed460338b0332e8e03fa75d6aa4e
Manguil <scorpion34700normand@gmail.com>
2022-10-23 16:26:04 +0200

    add

--- L2/Java/TESTJAVA/Te.java
+++
@@ -1,9 +0,0 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class Te {
-    public static void main(String[] args) {
-        Date date = new Date();
-        System.out.println(date);
-    }
-}
