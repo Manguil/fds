495d11b25991ac9013ec1d87bd6e229f6f77f0b2
Manguil <scorpion34700normand@gmail.com>
2022-10-25 10:16:26 +0200

    add

---
+++ L2/C/exo66.c
@@ -0,0 +1,11 @@
+#include <dirent.h>
+
+int main(int argc, char* argv[]) {
+    int n = ;
+    DIR* dossier = opendir();
+    struct dirent* entree = readdir(dossier);
+    while() {
+
+    }
+    return 0;
+}
