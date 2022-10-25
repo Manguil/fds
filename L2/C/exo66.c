884de0372c93e1c5ab5c434ed58a5cdf6d3e2068
Manguil <scorpion34700normand@gmail.com>
2022-10-25 11:12:14 +0200

    add

--- L2/C/exo66.c
+++ L2/C/exo66.c
@@ -1,11 +1,31 @@
 #include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-    int n = ;
-    DIR* dossier = opendir();
-    struct dirent* entree = readdir(dossier);
-    while() {
-
+    if(argc > 1) {
+        int n = 5;
+        DIR* dossier = opendir(argv[1]);;
+        struct dirent* entree;
+        struct stat info;
+        time_t now = time(NULL);
+        time_t modif;
+        int diff;
+        char* name;
+        if(dossier != NULL) {
+            while((entree = readdir(dossier)) != NULL) {
+                name = entree->d_name;
+                stat(name, &info);
+                modif = info.st_mtime;
+                diff = difftime(now, modif);
+                if(diff < n*24*60*60) {
+                    printf("%s\n", name);
+                }
+            }
+        }
     }
     return 0;
 }
