d9baa758f69b874cb1c72b3618006509fcdca527
Manguil <scorpion34700normand@gmail.com>
2023-09-19 11:16:00 +0200

    add

--- L2/S3/C/exo9.c
+++ L2/S3/C/exo9.c
@@ -5,16 +5,27 @@ char **strsplit(const char *s,const char sep)
 {
     int nbSep = 0;
     int compteNbCararc = 0;
-    for (int i = 0; s[i]!=NULL; i++)
+    for (int i = 0; s[i]; i++)
     {
-        if (s[i] == ":") nbSep ++;
+        if (s[i] == ':') nbSep ++;
         
     }
-    char* tab[nbSep+2]
-    for (int i = 0; s[i]!=NULL; i++)
+    char* tab[nbSep+2];
+    for (int i = 0; s[i]; i++)
     {
         int posTab = 0;
         if (s[i]==sep) posTab++;
-        else strncat(tab[posTab])
+        else strncat(tab[posTab],s,1);
+    }
+
+    return tab;
+}
+
+int main()
+{
+    char** tab = strsplit("/bin:/usr/bin:/usr/local/bin",':');
+    for(int i = 0;tab[i];i++)
+    {
+        printf("%i --> %s",i,tab[i]);
     }
 }
