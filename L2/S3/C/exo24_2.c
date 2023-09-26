6460c5bfaaa1f7a99a317a941d32487a19b8e5a3
Manguil <scorpion34700normand@gmail.com>
2023-09-26 11:15:12 +0200

    add

--- L2/S3/C/exo24_2.c
+++ L2/S3/C/exo24_2.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-int main(int argc, char *argv[], char *env[]) {
-    if(argc!=3){
-        printf("Syntaxe incorrecte : %s -[8|c123] fichier\n",argv[0]);
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("commmande format: %s -[8|c8] chemin\n", argv[0]);
         return 1;
     }
-    FILE *f=fopen(argv[2], "r");
-    if (f==NULL){
-        printf("Impossible d'ouvrir le fichier : %s \n",argv[2]);
+
+    FILE *f = fopen(argv[2],"r");
+    if (f == NULL) 
+    {
+        printf("erreur d'ouverture de %s\n",argv[2]);
         return 2;
     }
-    int nb; /* nb de lignes ou de car */
-    int CAR=0; /* bool CAR ou LIGNES */
-    if(argv[1][1]=='c'){ /* si n caractères */
-        CAR=1;
-        nb=atoi(argv[1]+2); /* +2 pour éviter le -c */
-    }else{
-        nb=atoi(argv[1]+1); /* +1 pour éviter le - */
+
+    int charactere = 0;
+    if (argv[1][1] == 'c') 
+    {
+        charactere = 1;
     }
-    if(CAR){
-        char tampon[nb+1];
-        int n=fread(tampon,1,nb,f); /* lire les nb car d'un coup */
-        tampon[n]='\0';
-        printf("%s",tampon);
-    }else{
-        #define TAILLE 2048
-        
-        char ligne[TAILLE]; /* espérons que cela suffise ! */
-        int FDF=0; /* Fin De Fichier */
-        for(int i=1;i<=nb && !FDF;i++){
-            if(fgets(ligne,TAILLE,f))
-                printf("%s",ligne);
-            else
-                FDF=1;
+
+    int nb = 0;
+    int nbALire = 0;
+    if (charactere)
+    {
+        nbALire = atoi(argv[1]+2); //pour sauter -c
+        char *tampon = malloc(sizeof(char)*nbALire);
+        if (fread(tampon, sizeof(char), nbALire, f) != nbALire) 
+        {
+            printf("erreur de lecture (surement fin de fichier)\n");
+            return 3;
+        }
+        printf("%s\n",tampon);
+        nb++;
+    }
+
+    /*
+    else 
+    {
+        nbALire = atoi(argv[1]+1); // pour sauter -
+        while (nb < nbALire)
+        {
+            if (fread(&c, sizeof(char), 1, f) != 1) 
+            {
+                printf("erreur de lecture (surement fin de fichier)\n");
+                return 3;
+            }
+            printf("%c",c);
+            nb++;
         }
     }
+    */
+
     fclose(f);
 }
